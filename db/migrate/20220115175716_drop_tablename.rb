28149d9529642cf50a6d3aa3661ced50a8a6f9fb
xtheslayer666 <xtheslayer666@gmail.com>
2022-01-16 19:11:09 +0100

    do   rake db:migrate

--- db/migrate/20220115175716_drop_tablename.rb
+++
@@ -1,6 +0,0 @@
-class DropTablename < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :fellowships
-    drop_table :fellowship_translations
-  end
-end
--- db/migrate/20220116083334_change_boolean_value.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBooleanValue < ActiveRecord::Migration[5.2]
-  def change
-    change_column :fellowships; :user_required_full_name, :boolean, default :false
-  end
-end
