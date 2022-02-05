cad2a44680a650ce77e1eaa9a103ced7d692f3c8
JanHass <janmika.hass@student.fhws.de>
2022-02-05 14:53:24 +0100

    Gruppenerstellung: +fehlende Checkbox + Übersetzungen EN und DE für alle Texte

--- app/models/fellowship.rb
+++ app/models/fellowship.rb
@@ -1,5 +1,8 @@
 class Fellowship < ApplicationRecord
-    has_one_attached :image
-    
+    has_one_attached :image   
+
+    def locale
+        self[:locale] ||= I18n.default_locale.to_s
+      end
 
 end
